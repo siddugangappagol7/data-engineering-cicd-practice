be4dc18c62dde3877eab41a4ece2d14afb11af02
siddugangappagol7 <siddugangappagol7@gmail.com>
2026-09-23 21:09:09 +0530

    Add Python CI pipeline

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, subtract, multiply
+
+
+def test_add():
+    assert add(10, 5) == 15
+
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+
+def test_multiply():
+    assert multiply(10, 5) == 50
